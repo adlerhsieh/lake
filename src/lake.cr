faff71daa13aee7f9e8ccab11723137e8b9339e6
Adler <nkj20932@hotmail.com>
2015-10-22 00:58:47 +0800

    allow copy, build, run all commands in .lake directory

--- src/lake.cr
+++ src/lake.cr
@@ -11,11 +11,25 @@ unless finder.lakefile
   abort("Error: Missing Lakefile".colorize(:red))
 end
 
-unless Dir.exists?("#{finder.root}/.lake")
-  Dir.mkdir("#{finder.root}/.lake")
-  Dir.mkdir("#{finder.root}/.lake/tasks") unless Dir.exists?("#{finder.root}/.lake/tasks")
-  Dir.mkdir("#{finder.root}/.lake/bin")   unless Dir.exists?("#{finder.root}/.lake/bin")
+Dir.mkdir("#{finder.root}/.lake")       unless Dir.exists?("#{finder.root}/.lake")
+Dir.mkdir("#{finder.root}/.lake/tasks") unless Dir.exists?("#{finder.root}/.lake/tasks")
+Dir.mkdir("#{finder.root}/.lake/bin")   unless Dir.exists?("#{finder.root}/.lake/bin")
+
+# File.read(finder.lakefile)
+
+Dir.entries("#{finder.root}/.lake").each do |file|
+  is_file = File.file?("#{finder.root}/.lake/#{file}")
+  is_cr   = File.extname(file) == ".cr"
+  if is_file && is_cr
+    builder = Lake::Builder.new("#{finder.root}/.lake/#{file}")
+    unless builder.match
+      builder.copy
+      builder.prepend_dsl
+      builder.build
+    end
+    # system(builder.file(:bin).to_s)
+  end
 end
 
-File.read(finder.lakefile)
+
 
--- src/lake/builder.cr
+++ src/lake/builder.cr
@@ -1,7 +1,38 @@
 module Lake
   class Builder
-    def initialize
-
+    def initialize(@file)
+      @task     = File.read(@file.to_s)
+      @root     = @file.split("/")[0..-2].join("/")
+      @filename = @file.split("/")[-1]
     end
+
+    def file(dir)
+      case dir
+      when :tasks
+        "#{@root}/tasks/#{@filename}"
+      when :bin
+        "#{@root}/bin/#{@filename[0..-4]}"
+      end
+    end
+
+    # Prepends Lake DSL to a task
+    def prepend_dsl
+      # WIP
+    end
+
+    # Matches task content with source
+    def match
+      @task == File.read(file(:tasks).to_s)
+    end
+
+    def copy
+      system("cp #{@file} #{file(:tasks)}")
+    end
+
+    # run crystal build and move it to bin directory
+    def build
+      system("crystal build #{file(:tasks)} -o #{file(:bin)}")
+    end
+
   end
 end
