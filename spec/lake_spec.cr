ea59cd7379ed8747670440b5f4ca6d793c9d7a1a
Adler <nkj20932@hotmail.com>
2015-10-25 19:37:26 +0800

    add purge and options spec

--- spec/lake_spec.cr
+++ spec/lake_spec.cr
@@ -1,8 +1,16 @@
 require "./spec_helper"
 
-system("rm lake")
-system("rm -rf .lake")
-system("rm Lakefile")
+describe "Purge" do
+  it "all files" do
+    system("./lake -p")
+    File.directory?("#{ENV["PWD"]}/.lake").should be_false
+    File.file?("#{ENV["PWD"]}/Lakefile").should   be_false
+  end
+  it "the executable" do
+    system("rm lake")
+    File.file?("#{ENV["PWD"]}/lake").should be_false
+  end
+end
 
 describe "Lake" do
   it "builds lake executable" do
@@ -10,13 +18,13 @@ describe "Lake" do
     File.file?("#{ENV["PWD"]}/lake").should be_true
   end
 
-  it "creates directory and Lakefile" do
+  it "without option" do
     system("./lake")
     File.file?("#{ENV["PWD"]}/Lakefile").should   be_true
     File.directory?("#{ENV["PWD"]}/.lake").should be_true
   end
 
-  it "creates tasks" do
+  it "# creates tasks" do
     File.write("#{ENV["PWD"]}/.lake/hello.cr",
                "Task.hello\n"\
                "puts \"hello world\"\n"\
@@ -31,7 +39,7 @@ describe "Lake" do
     File.file?("#{ENV["PWD"]}/.lake/go.cr").should    be_true
   end
 
-  it "builds tasks" do
+  it "-b" do
     system("./lake -b")
     File.file?("#{ENV["PWD"]}/.lake/tasks/hello.cr").should  be_true
     File.file?("#{ENV["PWD"]}/.lake/tasks/salute.cr").should be_true
@@ -39,5 +47,16 @@ describe "Lake" do
     File.file?("#{ENV["PWD"]}/.lake/bin/hello").should       be_true
     File.file?("#{ENV["PWD"]}/.lake/bin/salute").should      be_true
     File.file?("#{ENV["PWD"]}/.lake/bin/go").should          be_true
+  end
+end
+
+describe "options" do
+  it "exit with 1 without an option" do
+    system("./lake")
+    system("echo $?").should be_true
+  end
+  it "exit with 1 with an option" do
+    system("./lake -h")
+    system("echo $?").should be_true
   end
 end
